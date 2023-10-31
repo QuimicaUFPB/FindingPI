0f70b588e7674232c12c9ce641b8829749395f78
Joabson-110 <149449130+Joabson-110@users.noreply.github.com>
2023-10-31 14:18:23 -0300

    Update Graficos.py

--- Graficos/Graficos.py
+++ Graficos/Graficos.py
@@ -2,15 +2,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-## Parâmetros onde x e circunferência ao quadrado e y superfície.
-x = [3600, 19600, 29584, 45369, 82369]
+## Parâmetros onde x é circunferência, x1 é circunferência ao quadrado e y superfície.
+x = [60, 140, 172, 213, 287]
+x1 = [3600, 19600, 29584, 45369, 82369]
 y = [350, 1625, 2300, 3750, 6475]
 
-## Gráfico da circunferência ao qquadrado e superfície
-
+## Gráfico da circunferência e superfície
 plt.scatter(x,y)
-plt.xlabel('Circunferência ao quadrado')
+plt.xlabel('Circunferência')
 plt.ylabel('Superfície')
 plt.title('Gráfico da circunferência')
+plt.show()
+
+## Gráfico da circunferência ao quadrado e superfície
+
+plt.scatter(x1,y)
+plt.xlabel('Circunferência ao quadrado')
+plt.ylabel('Superfície')
+plt.title('Gráfico da circunferência ao quadrado')
 plt.show()
 ## plt.savefig('local/grafico.png')

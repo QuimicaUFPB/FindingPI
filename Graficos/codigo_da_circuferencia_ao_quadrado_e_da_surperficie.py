93bfa013ebebe3d8fd08698c135194faa17b123f
IsabeliSouto <149511454+IsabeliSouto@users.noreply.github.com>
2023-11-03 16:33:08 -0300

    codigo_da_circuferencia_ao_quadrado_e_da_surperficie.py

--- Graficos/codigo_da_circuferencia_ao_quadrado_e_da_surperficie.py
+++ Graficos/codigo_da_circuferencia_ao_quadrado_e_da_surperficie.py
@@ -6,3 +6,11 @@ import numpy as np
 x = [60, 140, 172, 213, 287, 25, 95, 124, 212, 285, 321]
 x1 = [3600, 19600, 29584, 45369, 82369, 625, 9025, 15376, 44944, 81225, 103041]
 y = [350, 1625, 2300, 3750, 6475, 50.0, 656.9, 1114.4, 3225.0, 6243.8, 7625.0]
+
+## Gráfico da circunferência ao quadrado e superfície
+
+plt.scatter(x1,y)
+plt.xlabel('Circunferência ao quadrado')
+plt.ylabel('Superfície')
+plt.grid()
+plt.savefig('/content/sample_data/grafico_circunferencia_quadrado.png')

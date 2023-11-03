97aed734422c8efe69b48d3ec12bdf453e6f4a88
IsabeliSouto <149511454+IsabeliSouto@users.noreply.github.com>
2023-11-03 16:23:48 -0300

    Update and rename codigodacircuferenciaesurperficie.py to deletecodigodacircuferenciaesurperficie.py

--- codigodacircuferenciaesurperficie.py
+++
@@ -1,8 +0,0 @@
-# Biblioteca
-import matplotlib.pyplot as plt
-import numpy as np
-
-## Parâmetros onde x é circunferência, x1 é circunferência ao quadrado e y superfície.
-x = [60, 140, 172, 213, 287, 25, 95, 124, 212, 285, 321]
-x1 = [3600, 19600, 29584, 45369, 82369, 625, 9025, 15376, 44944, 81225, 103041]
-y = [350, 1625, 2300, 3750, 6475, 50.0, 656.9, 1114.4, 3225.0, 6243.8, 7625.0]

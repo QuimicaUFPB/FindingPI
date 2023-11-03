fb0d49768ed4c944c11aa740bc49146942e2c1a8
IsabeliSouto <149511454+IsabeliSouto@users.noreply.github.com>
2023-11-03 16:30:28 -0300

    codigo_calculo_da_regressão_linear.py

--- Graficos/codigo_calculo_da_regressão_linear.py
+++ Graficos/codigo_calculo_da_regressão_linear.py
@@ -7,3 +7,13 @@ param=np.polyfit(x1,y,1) # ajuda a curva
 print('parâmetros: ',param) #mostra os parâmetros
 a, b = param
 print('reta: y = %.5f * x + %.5f' % (a, b))
+
+#gráfico da regressão linear
+x_fit=np.linspace(-0.1,103041.1,1000)
+y_fit=np.polyval(param,x_fit)
+plt.plot(x1,y,'r.',label='Dados Originais')
+plt.plot(x_fit,y_fit,'b--',label='Curva Ajustada')
+plt.xlabel('Circunferência ao quadrado')
+plt.ylabel('Superfície')
+plt.legend()
+plt.savefig('/content/sample_data/grafico_regressao_linear.png')

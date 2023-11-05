5eef8f280c03468616a1daa4f0c9149496f10bea
HelayneRodrigues <149539578+HelayneRodrigues@users.noreply.github.com>
2023-11-05 13:36:37 -0300

    Update codigo_da_circuferencia_ao_quadrado_e_da_surperficie.py

--- Graficos/codigo_da_circuferencia_ao_quadrado_e_da_surperficie.py
+++ Graficos/codigo_da_circuferencia_ao_quadrado_e_da_surperficie.py
@@ -10,7 +10,7 @@ y = [350, 1625, 2300, 3750, 6475, 50.0, 656.9, 1114.4, 3225.0, 6243.8, 7625.0]
 ## Gráfico da circunferência ao quadrado e superfície
 
 plt.scatter(x1,y)
-plt.xlabel('Circunferência ao quadrado (mm^2)')
-plt.ylabel('Superfície (mm^2)')
+plt.xlabel(xlabel=r'C^2 ($mm^2$)')
+plt.ylabel(ylabel=r'S ($mm^2$)')
 plt.grid()
 plt.savefig('/content/sample_data/grafico_circunferencia_quadrado.png')
